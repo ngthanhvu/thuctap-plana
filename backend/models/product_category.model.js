8dd4c59474b900546c2132a22995080ae53ea029
ngthanhvu <nguyenthanhvudvfb@gmail.com>
2025-06-05 09:16:59 +0700

    Update: he he

--- backend/models/product_category.model.js
+++ backend/models/product_category.model.js
@@ -1,40 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
-    const Category = sequelize.define('Category', {
-        id: { 
-            type: DataTypes.INTEGER, 
-            primaryKey: true, 
-            autoIncrement: true 
-        },
-        name: { 
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        parent_id: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            references: {
-                model: 'categories',
-                key: 'id'
-            }
-        },
+    const ProductCategory = sequelize.define('ProductCategory', {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        name: { type: DataTypes.STRING },
+        parent_id: { type: DataTypes.INTEGER },
         created_at: { type: DataTypes.DATE },
         updated_at: { type: DataTypes.DATE }
     }, {
-        tableName: 'categories',
-        timestamps: false
+        tableName: 'product_categories',
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
     });
 
-    Category.associate = (models) => {
-        Category.belongsTo(Category, {
-            as: 'parent',
-            foreignKey: 'parent_id'
-        });
-        
-        Category.hasMany(Category, {
+    ProductCategory.associate = (models) => {
+        ProductCategory.hasMany(models.ProductCategory, {
+            foreignKey: 'parent_id',
             as: 'children',
-            foreignKey: 'parent_id'
+        });
+
+        ProductCategory.belongsTo(models.ProductCategory, {
+            foreignKey: 'parent_id',
+            as: 'parent',
         });
     };
 
-    return Category;
+    return ProductCategory;
 };
