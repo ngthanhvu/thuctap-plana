5f9020df2b893bd7d67f06d744d60a2643f7db92
unknown <you@example.com>
2025-06-09 20:41:23 +0700

    update route edit category

--- frontend/src/components/Categories/EditCategoryForm.vue
+++ frontend/src/components/Categories/EditCategoryForm.vue
@@ -8,7 +8,8 @@
         <label for="name" class="block text-sm font-medium text-gray-700 mb-1">Tên danh mục</label>
         <input type="text" id="name" v-model="formData.name"
           class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          required />
+           />
+            <p v-if="errors.name" class="mt-1 text-sm text-red-600">{{ errors.name }}</p>
       </div>
 
       <!-- Danh mục cha -->
@@ -65,6 +66,9 @@ import { useRoute, useRouter } from 'vue-router'
 const route = useRoute()
 const router = useRouter()
 const categoryId = route.params.id
+const errors = ref({
+  name: ''
+});
 
 const formData = ref({
   name: '',
@@ -102,10 +106,25 @@ onMounted(async () => {
     console.error('Lỗi khi tải danh mục:', err)
     alert('Không thể tải dữ liệu danh mục')
   }
-})
+});
+
+//valid form 
+const validateForm = () => {
+  let isValid = true;
+  errors.value.name = '';
+  if(formData.value.name.trim() === '') {
+    errors.value.name = 'Danh mục không được để trống';
+    isValid = false;
+  }else if(formData.value.name.trim().length < 3) {
+    errors.value.name = 'Danh mục phải có ít nhất 3 ký tự';
+    isValid = false;
+  }
+  return isValid;
+}
 
 // Gửi API cập nhật
 const handleUpdate = async () => {
+  if(!validateForm()) return;
   try {
     const res = await fetch(`http://localhost:3000/api/categories/${categoryId}`, {
       method: 'PUT',
